e06e7538ffecc288a0cd2c3763f2ac89350e015f
abrahem haj ali <boss.abrahem@hotmail.com>
2024-08-13 20:11:45 +0300

    removed the old icon and fixed the position of items in the page

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
 
   return (
     <>
-      <main className="bg-gradient-to-b from-primary to-secondary h-screen w-full py-10 flex justify-center items-center">
+      <main className="bg-gradient-to-b from-primary to-secondary h-screen w-full py-10 flex flex-col justify-center items-center">
         <div className="">
           <CardWithForm pergolaTypes={types} />
         </div>
-        <div className="absolute bottom-8 text-white grid gap-3">
+        <div className="mt-16 bottom-8 text-white grid gap-3">
           <Image className="mx-auto" src={LogoWhite} alt="logo" priority />
           <p>כל הזכיות שמורות | סמארט פרגולה | {currentYear}</p>
         </div>
