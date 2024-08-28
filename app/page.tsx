bebd8678a9ff86ef53c266311fd9e2085ccb7807
abrahem haj ali <boss.abrahem@hotmail.com>
2024-08-28 18:44:40 +0300

    changed the position settings of the items

--- app/page.tsx
+++ app/page.tsx
@@ -11,20 +11,14 @@ export default function Home() {
   return (
     <>
       <main className="w-full py-10 flex flex-col justify-center items-center">
-        <div className="">
+        <section className="">
           <CardWithForm pergolaTypes={types} />
-        </div>
-        <div className="mt-16 bottom-8 text-white grid gap-3">
-          <Image
-            className="mx-auto"
-            src={logo}
-            alt="logo"
-            priority
-            width={160}
-          />
-          <p>כל הזכיות שמורות סמארט פרגולה {currentYear} &copy;</p>
-        </div>
+        </section>
       </main>
+      <section className="mt-16 bottom-8 text-white grid gap-3 fixed inset-x-1/2 min-w-[300px] translate-x-2/4">
+        <Image className="mx-auto" src={logo} alt="logo" priority width={100} />
+        <p>כל הזכיות שמורות סמארט פרגולה {currentYear} &copy;</p>
+      </section>
     </>
   );
 }
