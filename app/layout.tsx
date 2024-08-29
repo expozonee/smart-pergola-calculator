d92127aacf921339d8c6b83618011577e8ac3ebc
abrahem haj ali <boss.abrahem@hotmail.com>
2024-08-29 08:33:10 +0300

    added color for the browser ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Noto_Kufi_Arabic } from "next/font/google";
@@ -11,6 +11,10 @@ const rubikText = Rubik({ weight: ["500"], subsets: ["hebrew"] });
 export const metadata: Metadata = {
   title: "מחשבון מחיר | סמארט פרגולה",
   description: "מחשבון מחיר סמארט פרגולה",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#0098a7",
 };
 
 export default function RootLayout({
